42d5cecc6094764059a933de646113d279f0443c
tulsi-lukhi <tulsi.lukhi@techholding.co>
2024-05-07 17:39:46 +0530

    refactor: removed unused imports

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -5,10 +5,8 @@ import { UserButton } from "@clerk/nextjs";
 import React, { ReactNode } from "react";
 import styles from "./../../styles/dashboardLayout.module.css"
 import { Button } from "@/components/ui/button";
-import { useRouter } from 'next/navigation'
 
 function Layout({ children }: { children: ReactNode }) {
-  const router = useRouter()
   return (
     <div className={styles.container}>
       <nav className={styles.nav}>
--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-import React from "react";
 import styles from "../styles/logo.module.css";
 function Logo() {
   return (
